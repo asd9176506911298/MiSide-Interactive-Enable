11adde650afd23985346bc000840da0f3bae16b6
Yuki.kaco <a0936660023@gmail.com>
2025-01-06 01:23:06 +0800

    Update

--- UI/UIManager.cs
+++ UI/UIManager.cs
@@ -42,7 +42,88 @@ namespace InteractiveEnable.UI
         {
             if (Input.GetKeyDown(Plugin.Instance.ShowMenuKey.Value))
             {
-                Enabled = !Enabled;
+                //Enabled = !Enabled;
+
+                foreach (var obj in GameObject.FindObjectsOfType<MeshRenderer>())
+                {
+                    if (!(obj.gameObject.TryGetComponent<BoxCollider>(out var collider)))
+                    {
+                        var x = obj.gameObject.AddComponent<BoxCollider>();
+                        x.isTrigger = true;
+                    }
+                }
+
+                Camera mainCamera = GlobalTag.cameraPlayer.GetComponent<Camera>();
+                if (mainCamera == null)
+                {
+                    Plugin.Log.LogWarning("Main camera not found!");
+                    return;
+                }
+
+                Vector3 centerScreenPosition = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+
+                // Create a ray from the center of the screen
+                Ray ray = mainCamera.ScreenPointToRay(centerScreenPosition);
+
+                // Store hit information
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    // Log the name of the hit GameObject
+                    GameObject hitObject = hit.collider.gameObject;
+                    Plugin.Log.LogWarning($"Hit GameObject: {hitObject.name}");
+
+                    // Example: Add custom behavior to the hit object
+                    //hitObject.GetComponent<Renderer>()?.material.SetColor("_Color", Color.red);
+                    //GlobalTag.player.GetComponent<PlayerMove>().RemoveItem();
+                    if (hitObject != null)
+                    {
+                        ObjectItem o = null;
+                        if (!(hitObject.TryGetComponent<ObjectItem>(out var c)))
+                        {
+                            o = hitObject.AddComponent<ObjectItem>();
+                            //o.castBack = 0f;
+                            //o.castBackDistance = 10f;
+                            o.itemInHand = hitObject;
+                        }
+
+                        foreach (var x in Resources.FindObjectsOfTypeAll(Il2CppSystem.Type.GetType("PlayerHandIK_Prefab")))
+                        {
+                            if (o != null)
+                            {
+                                o.mainHand = x.TryCast<PlayerHandIK_Prefab>();
+                                break;
+                            }
+                        }
+                        GlobalTag.player.GetComponent<PlayerMove>().TakeItem(hitObject);
+
+                    }
+                    else
+                    {
+                        Plugin.Log.LogWarning("No GameObject hit by the raycast.");
+                    }
+
+                    //GlobalTag.player.GetComponent<PlayerMove>().RemoveItem();
+                    //if(GameObject.Find("World/House/Room Start/InfinityRoom/Decorations/DigitalClock") != null)
+                    //{
+                    //    var o = GameObject.Find("World/House/Room Start/InfinityRoom/Decorations/DigitalClock").AddComponent<ObjectItem>();
+                    //    o.castBack = 0.2f;
+                    //    o.castBackDistance = 1f;
+                    //    o.itemInHand = GameObject.Find("World/House/Room Start/InfinityRoom/Decorations/DigitalClock");
+
+                    //    foreach(var x in Resources.FindObjectsOfTypeAll(Il2CppSystem.Type.GetType("ObjectItem"))) {
+                    //        Plugin.Log.LogInfo(x);
+                    //        var objectItem = x.TryCast<ObjectItem>();
+                    //        Plugin.Log.LogInfo(objectItem);
+                    //        Plugin.Log.LogInfo(objectItem.mainHand);
+                    //        if (objectItem.mainHand != null)
+                    //            o.mainHand = objectItem.mainHand;
+                    //    }
+
+
+                    //    GlobalTag.player.GetComponent<PlayerMove>().TakeItem(GameObject.Find("World/House/Room Start/InfinityRoom/Decorations/DigitalClock"));
+                    //}
+                }
+
             }
         }
     }
